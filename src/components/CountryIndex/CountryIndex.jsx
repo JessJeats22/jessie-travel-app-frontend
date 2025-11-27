ff3c5253c5a4230321d5cfdea46cf97386a5a927
JessieJ <jvwiltshire22@gmail.com>
2025-11-27 14:48:28 +0000

    feat: scaffold TravelPostDetails component with initial placeholder

--- src/components/CountryIndex/CountryIndex.jsx
+++ src/components/CountryIndex/CountryIndex.jsx
@@ -1,51 +1,55 @@
 import './CountryIndex.css'
 import { useEffect, useState } from 'react'
-import {countryIndex} from '../../services/country'
+import { countryIndex } from '../../services/country'
 
 import { Link } from 'react-router'
 import LoadingIcon from '../LoadingIcon/LoadingIcon'
 
 const CountryIndex = () => {
-    const [countries, setCountries] = useState ([])
-    const [errorData, setErrorData] = useState({})
-    const [isLoading, setIsLoading] = useState(true)
+  const [countries, setCountries] = useState([])
+  const [errorData, setErrorData] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
 
 
 
   useEffect(() => {
     const fetchCountries = async () => {
-        try {
-            const {data} = await countryIndex()
-            setCountries(data)
-        } catch (error) {
-            console.log(error)
-            setErrorData(error.response.data)
-        } finally {
-            setIsLoading(false)
-        }
+      try {
+        const { data } = await countryIndex()
+        setCountries(data)
+      } catch (error) {
+        console.log(error)
+        setErrorData(error.response.data)
+      } finally {
+        setIsLoading(false)
+      }
     }
     fetchCountries()
   }, []);
 
-  return(
-  <>
+  return (
+    <>
 
-   <h1>All Countries</h1>
-   { errorData.message
+      <h1>All Countries</h1>
+      {errorData.message
         ? <p className='error-message'>{errorData.message}</p>
         : isLoading
-        ? <LoadingIcon />
-        : countries.map(country => {
-              return (
-                <div key={country._id} className='country-card'>
+          ? <LoadingIcon />
+          : (
+            <div className="country-grid">
+              {countries.map(country => (
+                <div key={country._id} className="country-card">
                   <Link to={`/countries/${country._id}`}>
                     <h2>{country.name}</h2>
-                    <p>{country.description.substring(0, 15)}...</p>
+                    <p>{country.description}...</p>
                   </Link>
                 </div>
-              )
-            })
+              ))}
+            </div>
+
+          )
       }
+
     </>
   )
 }
---
+++ src/components/TravelPostDetails/TravelPostDetails.jsx
@@ -0,0 +1,7 @@
+import './TravelPostDetails.css'
+
+const TravelPostDetails = () => {
+    return <h1>Travel Post Details!</h1>
+}
+
+export default TravelPostDetails
